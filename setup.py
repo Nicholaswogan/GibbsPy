99e2dddef09384ac790a1d02b124c53f82b46e5b
Nicholaswogan <wogan@uw.edu>
2020-04-11 14:29:03 -0700

    update

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GibbsPy',
+      version='1.0',
+      description='For calculating Gibbs Free Energies',
+      author='Nicholas Wogan',
+      author_email='nicholaswogan@gmail.com',
+      packages=['GibbsPy'],
+      include_package_data=True)
